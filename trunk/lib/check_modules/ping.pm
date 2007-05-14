f760461b10c246851a4aadf5541b807faafadfb4
kovyrin <kovyrin@d1d46f10-b92b-0410-9d26-5b496d7faea2>
2007-05-14 01:24:41 +0000

    porting to solaris: work with external fping

--- trunk/lib/check_modules/ping.pm
+++ trunk/lib/check_modules/ping.pm
@@ -3,8 +3,16 @@ sub PerformCheck($$) {
 
     my $ip = $config->{host}->{$host}->{ip};
     return "ERROR: Invalid host" unless ($ip);
+
+    # Find appropriate fping version
+    my $fping_path = "$SELF_DIR/bin/sys/fping";
+    chomp($fping_path = `which fping`) unless  (-f $fping_path && -x $fping_path && $^O eq 'linux');
+
+    unless (-f $fping_path && -x $fping_path) {
+        return "ERROR: fping is not functional - please, install your own version of fping on this server!";
+    }
     
-    my $res = `$SELF_DIR/bin/sys/fping -q -u -t 500 -C 1 $ip 2>&1`;
+    my $res = `$fping_path -q -u -t 500 -C 1 $ip 2>&1`;
     return "ERROR" if ($res =~ /$ip.*\-$/);
     return "OK";
 }
