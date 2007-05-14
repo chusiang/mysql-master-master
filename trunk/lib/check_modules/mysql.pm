876d2533530327ab4ca2676944fa95253ef0d085
kovyrin <kovyrin@d1d46f10-b92b-0410-9d26-5b496d7faea2>
2007-05-14 01:20:03 +0000

    Really working timeouts in mysql checks

--- trunk/lib/check_modules/mysql.pm
+++ trunk/lib/check_modules/mysql.pm
@@ -15,21 +15,36 @@ sub PerformCheck($$) {
     my $user = $peer->{user};
     my $pass = $peer->{password};
     
-    # connect to server
-    my $dsn = "DBI:mysql:host=$host;port=$port";
-    my $dbh = DBI->connect($dsn, $user, $pass, { PrintError => 0 });
+    eval {
+        local $SIG{ALRM} = sub { die "TIMEOUT"; };
+        alarm($timeout);
+        
+        # connect to server
+        my $dsn = "DBI:mysql:host=$host;port=$port;mysql_connect_timeout=$timeout";
+        my $dbh = DBI->connect($dsn, $user, $pass, { PrintError => 0 });
+        
+        unless ($dbh) {
+            alarm(0);
+            return "ERROR: Connect error (host = $host:$port, user = $user, pass = '$pass')! " . DBI::errstr;
+        }
     
-    return "ERROR: Connect error (host = $host:$port, user = $user, pass = '$pass')! " . DBI::errstr unless ($dbh);
-    
-    # Check server (simple)
-    my $sth = $dbh->prepare("SELECT NOW()");
-    my $res = $sth->execute;
+        # Check server (simple)
+        my $sth = $dbh->prepare("SELECT NOW()");
+        my $res = $sth->execute;
+        unless($res) {
+            alarm(0);
+            return "ERROR: SQL Query Error: " . $dbh->errstr;
+        }
 
-    $sth->finish;
-    $dbh->disconnect();
-    $dbh = undef;
+        $sth->finish;
+        $dbh->disconnect();
+        $dbh = undef;
 
-    return "ERROR: SQL Query Error: " . $dbh->errstr unless($res);
+        alarm(0);
+    };
+
+    alarm(0);
+    return 'ERROR: Timeout' if ($@ =~ /^TIMEOUT/);
     return "OK";
 }
 
