82dc6514a25d9f7876290bd453a8f0675a7b0838
maciej.dobrzanski <maciej.dobrzanski@d1d46f10-b92b-0410-9d26-5b496d7faea2>
2009-02-19 10:09:39 +0000

    Adding a missing version.pm

---
+++ branches/maciek/lib/version.pm
@@ -0,0 +1,20 @@
+my $PROGRAM_NAME = 'MySQL Multi-Master Replication Manager';
+my $PROGRAM_SHORT = 'MMM';
+my $PROGRAM_VERSION_MAJOR = 1;
+my $PROGRAM_VERSION_MINOR = 2;
+my $PROGRAM_VERSION_PATCH = 3;
+
+sub PrintVersion()
+{
+  printf("%s\n", $PROGRAM_NAME);
+  printf("Version: %d.%d.%d\n", $PROGRAM_VERSION_MAJOR, $PROGRAM_VERSION_MINOR, $PROGRAM_VERSION_PATCH);
+}
+
+sub GetVersion()
+{
+  my $version;
+  sprintf($version, "Version: %d.%d.%d\n", $PROGRAM_VERSION_MAJOR, $PROGRAM_VERSION_MINOR, $PROGRAM_VERSION_PATCH);
+  return $version;
+}
+
+1;
